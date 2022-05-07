00ce3d8cf8210c7d10b0fcd2797df28c91c5ee06
temurmalik <maxsumovtimurmalik17@gmail.com>
2022-05-07 19:22:17 +0500

    Debug True

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g$-fxq&ol_5@=-5&l!=yudi-fit-tc5bw^pg)@+_97qoup9^p$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
